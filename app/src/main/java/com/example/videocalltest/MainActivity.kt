0d3db0412755523a1a62a94358869b1100f850dd
Fer457 <fernandonevothernandez@gmail.com>
2024-01-12 10:33:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/videocalltest/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.videocalltest
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.lifecycleScope
+import com.example.videocalltest.ui.theme.VideocalltestTheme
+import io.getstream.video.android.compose.permission.LaunchCallPermissions
+import io.getstream.video.android.compose.theme.VideoTheme
+import io.getstream.video.android.compose.ui.components.call.activecall.CallContent
+import io.getstream.video.android.compose.ui.components.call.renderer.FloatingParticipantVideo
+import io.getstream.video.android.compose.ui.components.video.VideoRenderer
+import io.getstream.video.android.core.GEO
+import io.getstream.video.android.core.RealtimeConnection
+import io.getstream.video.android.core.StreamVideoBuilder
+import io.getstream.video.android.model.User
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val userToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiTWFyYV9KYWRlIiwiaXNzIjoiaHR0cHM6Ly9wcm9udG8uZ2V0c3RyZWFtLmlvIiwic3ViIjoidXNlci9NYXJhX0phZGUiLCJpYXQiOjE3MDQ5NzkxNDQsImV4cCI6MTcwNTU4Mzk0OX0.zQQdWgcu6C0Nx85-LMlyQV0fE0dRQWYhJalaXQIqgJY"
+        val userId = "Fer"
+        val callId = "7R3g2sL9kP1x"
+
+        // step1 - create a user.
+        val user = User(
+            id = userId, // any string
+            name = "Tutorial" // name and image are used in the UI
+        )
+
+        // step2 - initialize StreamVideo. For a production app we recommend adding the client to your Application class or di module.
+        val client = StreamVideoBuilder(
+            context = applicationContext,
+            apiKey = "mmhfdzb5evj2", // demo API key
+            geo = GEO.GlobalEdgeNetwork,
+            user = user,
+            token = userToken,
+        ).build()
+
+        // step3 - join a call, which type is `default` and id is `123`.
+        val call = client.call("default", callId)
+        lifecycleScope.launch {
+            val result = call.join(create = true)
+            result.onError {
+                Toast.makeText(applicationContext, it.message, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        setContent {
+            LaunchCallPermissions(call = call)
+
+            VideoTheme {
+                CallContent(
+                    modifier = Modifier.fillMaxSize(),
+                    call = call,
+                    onBackPressed = { onBackPressed() },
+                )
+            }
+
+        }
+
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VideocalltestTheme {
+        Greeting("Android")
+    }
+}
